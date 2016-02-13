01335157505da9e1648c76023b2cf4a9c85b1d28
Jesus Vazquez Fernandez <jesus.vazquezf@estudiants.urv.cat>
2016-02-13 15:37:09 +0100

    Commit Jesus 1

---
+++ app/src/main/java/com/example/hendal/codeathonurv2016_agendaurv/Professors.java
@@ -0,0 +1,38 @@
+package com.example.hendal.codeathonurv2016_agendaurv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Professors extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.professors);
+
+
+        final String[] listaprofesores = new String[]{
+                "Jordi Duch","Pedro Garcia", "Xavier Mallafré", "Álex Arenas", "Santiago Romaní"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, listaprofesores);
+        ListView listprofesview = (ListView) findViewById(R.id.listaprofesoresView);
+        listprofesview.setAdapter(adapter);
+        listprofesview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Intent i = new Intent(BuscaUniversitat.this, Pais.class);
+                //startActivity(i);
+
+            }
+        });
+
+
+
+
+    }
+
+}
